7c4e85122b3d6be07e0ba733039124683051779d
n.shvora <n.shvora@raceit.ru>
2025-02-11 18:41:55 +0300

    add fib

--- main.py
+++ main.py
@@ -8,3 +8,34 @@ def func_2(x):
     return x % 2 == 0
 
 func_3 = lambda x: x % 2 == 0
+
+def func_4(list_a, list_b):
+    return list(set(list_a) & set(list_b))
+
+
+count = func_1(2)
+
+print(count)
+
+def factorial(n):
+    count = 1
+    for i in range(2, n + 1):
+        count *= i
+    return count
+
+# def factorial(n):
+#     print(n)
+#     if n == 0 or n == 1:
+#         return 1
+#     else:
+#         return n * factorial(n - 1)
+
+def fib(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fib(n - 1) + fib(n - 2)
+
+print(fib(7))
