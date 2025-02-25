3995739ac30a38855202b9935c2a9bd445f791c4
n.shvora <n.shvora@raceit.ru>
2025-02-25 18:56:18 +0300

    ff

--- main.py
+++ main.py
@@ -9,4 +9,15 @@ bot = telebot.TeleBot(BOT_TOKEN)
 def start_message(message):
     bot.reply_to(message,"Hello world!")
 
+@bot.message_handler(commands=['help'])
+def help_message(message):
+    bot.send_message(message.chat.id,"Привет! Я ничего не умею, но обязательно научусь")
+
+@bot.message_handler(content_types='text')
+def message_reply(message):
+    if message.text == "Привет! Пойдем гулять?":
+        bot.send_message(message.chat.id, f"Привет! Я не могу, у меня лапки")
+    else:
+        bot.send_message(message.chat.id, f"Я не знаю, что мне с этим делать(")
+
 bot.infinity_polling()
